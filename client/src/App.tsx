8df7dbdbfd34091ab7b55d4ad812803ce4ad3991
jonathan dante <jono420dante@gmail.com>
2026-03-28 13:13:53 +0200

    Fix routing structure for studio page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -11,8 +12,7 @@ import Pricing from "./pages/Pricing";
 import { useEffect } from "react";
 
 // Router - Studio is publicly accessible (no auth required on Vercel)
-function Router() {
-  return (
+function Routes() {  return (
     <Switch>
       <Route path={"/"} component={Home} />
       <Route path={"/studio"} component={StudioFunctional} />
@@ -46,9 +46,9 @@ function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
+          <Router hook={useHashLocation}>
+                      <Routes />
+          </Router>      </ThemeProvider>
     </ErrorBoundary>
   );
 }
